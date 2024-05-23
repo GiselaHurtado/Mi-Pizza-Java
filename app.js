a4e67dd5a2c7bbbe756a9eeef8ecefae6bd81ad6
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-23 10:10:26 +0200

    mezclar ingredientes

---
+++ app.js
@@ -0,0 +1,20 @@
+let levadura = 30;
+let aguaTibia = 2.5;
+let aceiteDeOliva = 2;
+let harinaDeFuerza = 1;
+let salFina = 1;
+
+//function mezclar(levadura, aguaTibia, aceiteDeOliva,HarinaDeFuerza, salFina ) {
+let mezclar = levadura, aguaTibia, aceiteDeOliva, HarinaDeFuerza, salFina;
+console.log("El resultado de la mezcla de " + "levadura")
+}
+
+let ingredientes = [["levadura", 30], ["aguaTibia", 2.5], ["aceiteDeOliva", 2], ["harinaDeFuerza", 2], ["salFina", 1]];
+
+console.log("voy a mezclar" + " " + ingredientes[0][1] + " " + "gramos de" + " " + ingredientes[0][0] + ".");
+console.log("voy a mezclar" + " " + ingredientes[1][1] + " " + "gramos de" + " " + ingredientes[0][0] + ".");
+
+
+
+
+
