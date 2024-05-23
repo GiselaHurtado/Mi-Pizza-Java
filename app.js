10d8e909b568861d073dc7cda3c2635f15278f10
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-23 11:02:49 +0200

    inredientes

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-let levadura = 30;
+let levadura = "30 gramos de levadura";
 let aguaTibia = 2.5;
 let aceiteDeOliva = 2;
 let harinaDeFuerza = 1;
 let salFina = 1;
 
 //function mezclar(levadura, aguaTibia, aceiteDeOliva,HarinaDeFuerza, salFina ) {
-let mezclar = levadura, aguaTibia, aceiteDeOliva, HarinaDeFuerza, salFina;
-console.log("El resultado de la mezcla de " + "levadura")
-}
+
 
 let ingredientes = [["levadura", 30], ["aguaTibia", 2.5], ["aceiteDeOliva", 2], ["harinaDeFuerza", 2], ["salFina", 1]];
 
-console.log("voy a mezclar" + " " + ingredientes[0][1] + " " + "gramos de" + " " + ingredientes[0][0] + ".");
-console.log("voy a mezclar" + " " + ingredientes[1][1] + " " + "gramos de" + " " + ingredientes[0][0] + ".");
+console.log("voy a mezclar" + " " + ingredientes[0][1] + " " + "gramos de" + " " + ingredientes[0][0] + ",");
+console.log("voy a mezclar" + " " + ingredientes[1][1] + " " + "tazas de" + " " + ingredientes[1][0] + ",");
+console.log("voy a mezclar" + " " + ingredientes[2][1] + " " + "cucharas soperas de" + " " + ingredientes[2][0] + ",");
+console.log("voy a mezclar" + " " + ingredientes[3][1] + " " + "kilos  de" + " " + ingredientes[3][0] + ",");
+console.log("y" + " " + ingredientes[4][1] + " " + "cucharadita de" + " " + ingredientes[4][0] + ".");
 
-
-
+console.log()
 
 
