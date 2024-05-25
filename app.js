9f98e30e4dfdc8a2581b825c62e48b735a3e96ea
“Gisela-Soledad-Hurtado” <hurtadogiselasoledad@gmail.com>
2024-05-25 17:18:47 +0200

    added part 1 of exercice MyPizza on JavasScript

--- app.js
+++ app.js
@@ -1,20 +1,8 @@
-let levadura = "30 gramos de levadura";
-let aguaTibia = 2.5;
-let aceiteDeOliva = 2;
-let harinaDeFuerza = 1;
-let salFina = 1;
+import { } from "./js/mypizza.js";
 
-//function mezclar(levadura, aguaTibia, aceiteDeOliva,HarinaDeFuerza, salFina ) {
+function app() {
 
+}
 
-let ingredientes = [["levadura", 30], ["aguaTibia", 2.5], ["aceiteDeOliva", 2], ["harinaDeFuerza", 2], ["salFina", 1]];
-
-console.log("voy a mezclar" + " " + ingredientes[0][1] + " " + "gramos de" + " " + ingredientes[0][0] + ",");
-console.log("voy a mezclar" + " " + ingredientes[1][1] + " " + "tazas de" + " " + ingredientes[1][0] + ",");
-console.log("voy a mezclar" + " " + ingredientes[2][1] + " " + "cucharas soperas de" + " " + ingredientes[2][0] + ",");
-console.log("voy a mezclar" + " " + ingredientes[3][1] + " " + "kilos  de" + " " + ingredientes[3][0] + ",");
-console.log("y" + " " + ingredientes[4][1] + " " + "cucharadita de" + " " + ingredientes[4][0] + ".");
-
-console.log()
-
+app()
 
---
+++ js/mypizza.js
@@ -0,0 +1,62 @@
+
+const levadura = '30 gramos de Levadura fresca';
+const agua = '2½ tazas de Agua tibia';
+const aceiteDeOliva = '2 cucharadas soperas de Aceite de Oliva';
+const harina = 'Harina de fuerza';
+const sal = '1 cucharadita de Sal fina';
+const tiempoReposo = '45 min';
+const tiempoHorneado = '10 min';
+
+function mezclarLevaduraConAgua() {
+    console.log(`Mezclar ${levadura} con ${agua}`);
+}
+
+function añadirAceite() {
+    console.log(`Añadir ${aceiteDeOliva}`);
+}
+
+function añadirHarinaYSal() {
+    console.log(`Añadir ${harina}`);
+    console.log(`Añadir ${sal}`);
+}
+
+function amasarMasa() {
+    console.log('Amasar bien hasta que deje de ser pegajosa');
+}
+
+function dividirMasa() {
+    console.log('Darle forma a la masa para 4 pizzas');
+    console.log('4 porciones obtenidas');
+}
+
+function prepararBase() {
+    console.log('Base para 1 pizza');
+    console.log(`Dejar la base para 1 pizza reposar durante ${tiempoReposo}`);
+    console.log('Base con forma circular obtenida');
+}
+
+function añadirIngredientes() {
+    console.log('Añadido Salsa de tomate');
+    console.log('Añadido Mozzarella');
+    console.log('Añadido Pepperoni');
+    console.log('Añadidas 4 Aceitunas Negras');
+}
+
+function hornearPizza() {
+    console.log(`Horneando la pizza durante ${tiempoHorneado}`);
+}
+
+function pizzaLista() {
+    console.log('Pizza lista');
+    console.log('Pizza Entregada');
+}
+
+mezclarLevaduraConAgua();
+añadirAceite();
+añadirHarinaYSal();
+amasarMasa();
+dividirMasa();
+prepararBase();
+añadirIngredientes();
+hornearPizza();
+pizzaLista();
